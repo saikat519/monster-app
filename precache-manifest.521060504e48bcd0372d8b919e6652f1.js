e8d6319e0a9c9178a42892a8256542abf47fd77a
saikat519 <saikat.mukhopadhyay17@egmail.com>
2021-01-14 19:07:41 +0530

    Updates

---
+++ precache-manifest.521060504e48bcd0372d8b919e6652f1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "504ab61e9715ef4a7c78f1f5d2fa09b9",
+    "url": "/monster-app/index.html"
+  },
+  {
+    "revision": "0ce02f985eb91895296e",
+    "url": "/monster-app/static/css/main.62d87302.chunk.css"
+  },
+  {
+    "revision": "6952c745d55c5a0fa7de",
+    "url": "/monster-app/static/js/2.e47ca8d3.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monster-app/static/js/2.e47ca8d3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0ce02f985eb91895296e",
+    "url": "/monster-app/static/js/main.94b6a432.chunk.js"
+  },
+  {
+    "revision": "70ffa8869a950765e1c3",
+    "url": "/monster-app/static/js/runtime-main.4edd5f74.js"
+  }
+]);
